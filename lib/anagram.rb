46660d7a01026f1e6bb6f57d93ee49221cf7b598
levideutsch <levideutsch@live.com>
2023-02-28 00:44:00 -0500

    commit tree adff3a1d0516b97f8d2e9172341f632a02e482b0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+    attr_accessor :match
+
+    def initialize(word, match)
+        @word = word
+        @match = match
+    end
+end
+
+listen = Anagram.new("something")
+
+binding.pry
